c54bb75f4e24405fff0b8c725ecbfa176e5ecc7c
Dora Palfi <dorapalfi@Doras-MacBook-Pro.local>
2016-04-17 21:32:42 +0400

    April 17, initial colorwheel code for psychopy with slight change

--- psychopy/app/builder/components/initial_wheel.py
+++ psychopy/app/builder/components/initial_wheel.py
@@ -5,9 +5,6 @@ import random
 #create a window
 mywin = visual.Window([800,600], monitor="testMonitor", units="deg")
 
-start=random.randint(1, 360)
-
-
 #building the colorwheel: you build 180 wedges, each take up two degree space (they range from 2x to 2x+2)
 #for their color you call the array/dictionary/file that stores the colormatrix from Matlab
 #you draw the wedges inside the loop but update the window outside the loop? 
@@ -16,20 +13,31 @@ my_matrix= loadmat('./colormatrix.mat')
 
 my_colormatrix=my_matrix['colormatrix']
 
+#parameters that users will be able to define, for now arbitrary values
+
+#size of the wheel
+my_size = [16,16]
+
+#size of the inner disc
+center_size = [11, 11]
+
+#the angle at which the progam starts building the wheel
+start=random.randint(1, 360)
+
 for x in xrange (0, 180):
     
-    colorwheel = visual.RadialStim(win=mywin, units='deg', size=[16, 16], radialCycles=0, angularCycles=0,
+    colorwheel = visual.RadialStim(win=mywin, units='deg', size=my_size, radialCycles=0, angularCycles=0,
         radialPhase=0, angularPhase= 0, ori= start, texRes = 50, angularRes=300, visibleWedge=(0, 360-2*x),
         color= tuple(my_colormatrix[x-1]), colorSpace='rgb255', contrast=1.0, opacity=1.0, depth=0, 
         rgbPedestal=(0, 0, 0), interpolate=False)
     colorwheel.draw()
     x=x+1
    
-gray_circle = visual.RadialStim(win=mywin, units='deg', size=[11, 11], radialCycles=0, angularCycles=0,
+middle_circle = visual.RadialStim(win=mywin, units='deg', size= center_size, radialCycles=0, angularCycles=0,
         radialPhase=0, angularPhase= 0, ori= start, texRes = 50, angularRes=300, visibleWedge=(0, 360),
         color= (126,126,126), colorSpace='rgb255', contrast=1.0, opacity=1.0, depth=0, 
         rgbPedestal=(0, 0, 0), interpolate=False)
-gray_circle.draw()
+middle_circle.draw()
 
 mywin.update()  
 
